980d948d73e24931cd9fb06d541f16c83ef6927f
Robert Walsh <20102479@mail.wit.ie>
2024-10-15 12:53:18 +0100

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -60,6 +60,17 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+      <Paper component="ul" sx={{...root}}>
+      <li>
+          <Chip label="Production Countries" sx={{...chip}}
+          color="primary"/>
+        </li>
+        {movie.production_countries.map((countries) => (
+          <li key={countries.name}>
+            <Chip label={countries.name} sx={{...chip}} />
+          </li>
+        ))}
+      </Paper>
       <Fab
         color="secondary"
         variant="extended"

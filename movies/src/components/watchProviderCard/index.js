d4a7f3149e4d0d4528003b6adf2de81d9d82fe73
Robert Walsh <20102479@mail.wit.ie>
2025-08-07 19:58:13 +0100

    Repeat - Visual Update to Watch Provider Card

--- movies/src/components/watchProviderCard/index.js
+++ movies/src/components/watchProviderCard/index.js
@@ -1,27 +1,36 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
-import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
 import img from '../../images/film-poster-placeholder.png';
 
 
 const WatchProviderCard = ({ results }) => {
     return (
-        <Card>
-            <CardHeader
-            title={
-                <Typography variant="h6" component="p">
-                    {results.provider_name}
-                </Typography>
-            }
-            subheader={results.type}
-            />
-            <CardMedia 
-            sx = {{height: 400}}
-            image = {results.logo_path ? `https://image.tmdb.org/t/p/w500/${results.logo_path}`: img}
+        <Card sx={{ display: 'flex', padding: 0, alignItems: 'center', backgroundColor: "#121212", color: "white" }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography component="div" variant="h5">
+                        {results.provider_name}
+                    </Typography>
+                    <Typography
+                        variant="subtitle1"
+                        component="div"
+                    >
+                        {results.type}
+                    </Typography>
+                </CardContent>
+            </Box>
+            <CardMedia
+                component="img"
+                sx={{ width: 100, padding: 1 }}
+                image={results.logo_path ? `https://image.tmdb.org/t/p/w500/${results.logo_path}` : img}
+                alt={results.provider_name}
             />
         </Card>
+
     );
 };
 export default WatchProviderCard;

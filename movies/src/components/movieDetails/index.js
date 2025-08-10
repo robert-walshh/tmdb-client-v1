cf4478807f5515f86fd511dd070a7b39497ca148
Robert Walsh <20102479@mail.wit.ie>
2025-08-10 13:51:13 +0100

    Repeat - Added message for No Providers

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -11,7 +11,7 @@ import Drawer from "@mui/material/Drawer";
 import MovieReviews from "../movieReviews"
 import { getWatchProviders } from "../../api/tmdb-api";
 import { getMovieCastList } from "../../api/tmdb-api";
-import { useQuery } from "react-query"; 
+import { useQuery } from "react-query";
 import Spinner from '../spinner';
 import { Grid } from "@mui/material";
 import WatchProviderCard from "../watchProviderCard";
@@ -20,12 +20,12 @@ import MovieCastList from "../movieCastList";
 
 
 const root = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: 1.5,
-    margin: 0,
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  listStyle: "none",
+  padding: 1.5,
+  margin: 0,
 };
 const chip = { margin: 0.5 };
 
@@ -33,29 +33,29 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
   const [drawerOpen, setDrawerOpen] = useState(false);
 
 
-  const {data: providers, isLoading: providersLoading, isError: providersError } = useQuery(
+  const { data: providers, isLoading: providersLoading, isError: providersError } = useQuery(
     ["movieProviders", movie.id],
     () => getWatchProviders(movie.id),
-    {enables: ! !movie.id}
+    { enables: ! !movie.id }
   );
 
-  const {data: cast, isLoading: castLoading, isError: castError } = useQuery(
-  ["movieCastList", movie.id],
-  () => getMovieCastList(movie.id),
-  { enabled: ! !movie.id}
-);
+  const { data: cast, isLoading: castLoading, isError: castError } = useQuery(
+    ["movieCastList", movie.id],
+    () => getMovieCastList(movie.id),
+    { enabled: ! !movie.id }
+  );
 
   if (providersLoading || castLoading) return <Spinner />; // Show spinner while loading
   if (providersError || castError) return <Typography>Error loading data</Typography>;
 
   const irishData = providers && providers.results && providers.results["IE"];
-const providersArray = irishData 
-  ? [
+  const providersArray = irishData
+    ? [
       ...(irishData.flatrate || []).map(provider => ({ ...provider, type: 'Subscription' })),
       ...(irishData.buy || []).map(provider => ({ ...provider, type: 'Buy' })),
       ...(irishData.rent || []).map(provider => ({ ...provider, type: 'Rent' }))
-    ] 
-  : [];
+    ]
+    : [];
 
   const castArray = cast?.cast || [];
 
@@ -72,20 +72,20 @@ const providersArray = irishData
         {movie.overview}
       </Typography>
 
-      <Paper 
-        component="ul" 
-        sx={{...root}}
+      <Paper
+        component="ul"
+        sx={{ ...root }}
       >
         <li>
-          <Chip label="Genres" sx={{...chip}} color="primary" />
+          <Chip label="Genres" sx={{ ...chip }} color="primary" />
         </li>
         {movie.genres.map((g) => (
           <li key={g.name}>
-            <Chip label={g.name} sx={{...chip}} />
+            <Chip label={g.name} sx={{ ...chip }} />
           </li>
         ))}
       </Paper>
-      <Paper component="ul" sx={{...root}}>
+      <Paper component="ul" sx={{ ...root }}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
           icon={<MonetizationIcon />}
@@ -97,48 +97,54 @@ const providersArray = irishData
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-      <Paper component="ul" sx={{...root}}>
-      <li>
-          <Chip label="Production Countries" sx={{...chip}}
-          color="primary"/>
+      <Paper component="ul" sx={{ ...root }}>
+        <li>
+          <Chip label="Production Countries" sx={{ ...chip }}
+            color="primary" />
         </li>
         {movie.production_countries.map((countries) => (
           <li key={countries.name}>
-            <Chip label={countries.name} sx={{...chip}} />
+            <Chip label={countries.name} sx={{ ...chip }} />
           </li>
         ))}
       </Paper>
 
-             <Typography variant="h5" component="h3" sx={{ marginTop: 3 }}>
+      <Typography variant="h5" component="h3" sx={{ marginTop: 3 }}>
         Cast
       </Typography>
       <Grid container spacing={2}>
         {castArray.map((cast, index) => (
           cast && (
-          <Grid item key={cast.cast_id || index} xs={12} sm={6} md={4} lg={3}>
-            <MovieCastList results={cast} />
-          </Grid>
-          )
-        ))}
-      </Grid>
-      
-      <Typography variant="h5" component="h3" sx={{ marginTop: 3 }}>
-        Providers
-      </Typography>
-      <Grid container spacing={2}>
-        {providersArray.map((provider, index) => (
-          provider && (
-          <Grid item key={provider.provider_id || index} xs={12} sm={6} md={4} lg={3}>
-            <WatchProviderCard results={provider} />
-          </Grid>
+            <Grid item key={cast.cast_id || index} xs={12} sm={6} md={4} lg={3}>
+              <MovieCastList results={cast} />
+            </Grid>
           )
         ))}
       </Grid>
 
+      <Typography variant="h5" component="h3" sx={{ marginTop: 3 }}>
+        Providers
+      </Typography>
+      {providersArray.length === 0 ? (   // Checks if providersArray is empty
+        <Typography variant="h5" align="center">
+          No Providers Available
+        </Typography>
+      ) : (
+        <Grid container spacing={2}>
+          {providersArray.map((provider, index) => (
+            provider && (
+              <Grid item key={provider.provider_id || index} xs={12} sm={6} md={4} lg={3}>
+                <WatchProviderCard results={provider} />
+              </Grid>
+            )
+          ))}
+        </Grid>
+      )}
+
       <Fab
         color="secondary"
         variant="extended"
-        onClick={() =>setDrawerOpen(true)}
+        onClick={() => setDrawerOpen(true)}
         sx={{
           position: 'fixed',
           bottom: '1em',
@@ -151,7 +157,7 @@ const providersArray = irishData
       <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
         <MovieReviews movie={movie} />
       </Drawer>
-      </>
+    </>
   );
 };
-export default MovieDetails ;
+export default MovieDetails;
